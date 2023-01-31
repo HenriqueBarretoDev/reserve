96292ec21f216bb4ce128982a6d16cb5b2c6a968
Henrique Barreto <77741199+HenriqueBarretoDev@users.noreply.github.com>
2023-01-31 10:16:59 -0300

    ajuste na branch 8

--- reserve-e-corte/src/Pages/Home/index.jsx
+++ reserve-e-corte/src/Pages/Home/index.jsx
@@ -1,143 +1,143 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  ContainerSchedules,
-  IconsLeftSchedules,
-  IconsRightSchedules,
-  BoxTime,
-  Reservations,
-  MainReservation,
+    ContainerSchedules,
+    IconsLeftSchedules,
+    IconsRightSchedules,
+    BoxTime,
+    Reservations,
+    MainReservation,
 } from './styles';
-import { BsFillCalendar2EventFill } from 'react-icons/bs';
-import { IoEllipsisVerticalSharp } from 'react-icons/io5';
+import {BsFillCalendar2EventFill} from 'react-icons/bs';
+import {IoEllipsisVerticalSharp} from 'react-icons/io5';
 import HamburguerIcon from '../../Components/Hamburguer';
 
 const Schedules = () => {
-  const [currentDay, setCurrentDay] = useState(new Date().getDay());
-  const [day, setDay] = useState(
-    new Date().toLocaleString('pt-BR', { weekday: 'long' }),
-  );
+    const [currentDay, setCurrentDay] = useState(new Date().getDay());
+    const [day, setDay] = useState(
+        new Date().toLocaleString('pt-BR', {weekday: 'long'}),
+    );
 
-  function updateCurrentDay() {
-    setCurrentDay(new Date().getDay());
-  }
+    function updateCurrentDay() {
+        setCurrentDay(new Date().getDay());
+    }
 
-  useEffect(() => {
-    updateCurrentDay();
-  }, []);
+    useEffect(() => {
+        updateCurrentDay();
+    }, []);
 
-  const [date, setDate] = useState(new Date());
-  const [selectedTime, setSelectedTime] = useState(null);
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [date, setDate] = useState(new Date());
+    const [selectedTime, setSelectedTime] = useState(null);
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  const today = new Date();
+    const today = new Date();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
-  function renderCalendar() {
-    // render calendar
-  }
+    function renderCalendar() {
+        // render calendar
+    }
 
-  function renderAvailableTimes() {
-    // render available times
-  }
+    function renderAvailableTimes() {
+        // render available times
+    }
 
-  function handleDateSelection(newDate) {
-    setDate(newDate);
-  }
+    function handleDateSelection(newDate) {
+        setDate(newDate);
+    }
 
-  function handleTimeSelection(newTime) {
-    setSelectedTime(newTime);
-  }
+    function handleTimeSelection(newTime) {
+        setSelectedTime(newTime);
+    }
 
-  return (
-    <ContainerSchedules>
-      <header>
-        <div>
-          <HamburguerIcon />
-        </div>
-        <IconsLeftSchedules>
-          <h1>{day}</h1>
-        </IconsLeftSchedules>
+    return (
+        <ContainerSchedules>
+            <header>
+                <div>
+                    <HamburguerIcon/>
+                </div>
+                <IconsLeftSchedules>
+                    <h1>{day}</h1>
+                </IconsLeftSchedules>
 
-        <IconsRightSchedules>
-          <div>
-            <BsFillCalendar2EventFill />
-          </div>
-          <div>
-            <IoEllipsisVerticalSharp />
-          </div>
-        </IconsRightSchedules>
-      </header>
-      <h1>Reserve seu horário</h1>
+                <IconsRightSchedules>
+                    <div>
+                        <BsFillCalendar2EventFill/>
+                    </div>
+                    <div>
+                        <IoEllipsisVerticalSharp/>
+                    </div>
+                </IconsRightSchedules>
+            </header>
+            <h1>Reserve seu horário</h1>
 
-      <MainReservation>
-        <BoxTime>
-          <p>08:00</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#acf232' }}>
-          <p>Horário disponível</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>08:00</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#acf232'}}>
+                    <p>Horário disponível</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>08:30</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#acf232' }}>
-          <p>Horário disponível</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>08:30</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#acf232'}}>
+                    <p>Horário disponível</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>09:00</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#e21c34' }}>
-          <p>Horário Ocupado</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>09:00</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#e21c34'}}>
+                    <p>Horário Ocupado</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>09:30</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#e21c34' }}>
-          <p>Horário Ocupado</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>09:30</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#e21c34'}}>
+                    <p>Horário Ocupado</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>10:00</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#acf232' }}>
-          <p>Horário disponível</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>10:00</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#acf232'}}>
+                    <p>Horário disponível</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>10:30</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#acf232' }}>
-          <p>Horário disponível</p>
-        </Reservations>
-      </MainReservation>
+            <MainReservation>
+                <BoxTime>
+                    <p>10:30</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#acf232'}}>
+                    <p>Horário disponível</p>
+                </Reservations>
+            </MainReservation>
 
-      <MainReservation>
-        <BoxTime>
-          <p>10:30</p>
-        </BoxTime>
-        <Reservations style={{ backgroundColor: '#e21c34' }}>
-          <p>Horário Ocupado</p>
-        </Reservations>
-      </MainReservation>
-    </ContainerSchedules>
-  );
+            <MainReservation>
+                <BoxTime>
+                    <p>10:30</p>
+                </BoxTime>
+                <Reservations style={{backgroundColor: '#e21c34'}}>
+                    <p>Horário Ocupado</p>
+                </Reservations>
+            </MainReservation>
+        </ContainerSchedules>
+    );
 };
 export default Schedules;
