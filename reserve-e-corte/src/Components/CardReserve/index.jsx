25abe24defc9cd102e5bbeb5fe3051e8321a9723
Henrique Barreto <77741199+HenriqueBarretoDev@users.noreply.github.com>
2023-02-02 18:43:56 -0300

    ajuster no layout do card whatsapp e nome;implementado funções do card whatsapp e nome

--- reserve-e-corte/src/Components/CardReserve/index.jsx
+++ reserve-e-corte/src/Components/CardReserve/index.jsx
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react'
 import {BoxTime, ButtonSend, InputMenu, MainReservation, Reservations} from "./styles";
 import {useAuth} from "../../Hooks/useAuth";
 import moment from 'moment';
-import {TbMoodHappy} from "react-icons/tb"
-import {BsWhatsapp} from "react-icons/bs"
-import {AiOutlineUser} from "react-icons/ai"
-import {SlClose} from "react-icons/sl";
+import {BsWhatsapp, BsCheckCircle} from "react-icons/bs"
+import {AiOutlineUser, AiOutlineArrowRight} from "react-icons/ai"
+import {GiPadlock} from "react-icons/gi";
+import ButtonSendRegister from '../../Components/ButtonSendRegister'
 
 const CardReserve = ({cardTimer}) => {
 
@@ -22,7 +22,7 @@ const CardReserve = ({cardTimer}) => {
     const [reservedTime, setReservedTime] = useState(false);
     const [notReservation, setNotReservation] = useState(true);
     const [customerWithCompletedAppointment, setCustomerWithCompletedAppointment] = useState(false);
-
+    const [name, setName] = useState('');
     const {time, setTime} = useAuth(10)
 
     const handleChangeNumberPhone = event => {
@@ -34,7 +34,6 @@ const CardReserve = ({cardTimer}) => {
         setPhone(newValue);
     };
 
-
     const handleCodeChange = (event) => {
         setCode(event.target.value);
     };
@@ -42,7 +41,6 @@ const CardReserve = ({cardTimer}) => {
     const validateCode = (code) => {
         return code.length === 6;
     };
-
 
     const handleCodeValidation = () => {
         if (validateCode(code)) {
@@ -52,7 +50,7 @@ const CardReserve = ({cardTimer}) => {
             setConfirmWithCode(false)
             setNotReservation(false)
             setCustomerWithCompletedAppointment(false)
-            setTime(8)
+            setTime(10)
         } else {
             setValidationMessage('Código inválido');
         }
@@ -64,8 +62,13 @@ const CardReserve = ({cardTimer}) => {
     }
 
     function startWhatsappValidation() {
-        setAvailableTime(false)
-        setConfirmWithCode(true)
+        if (name && phone) {
+            setAvailableTime(false)
+            setConfirmWithCode(true)
+        } else {
+            return
+        }
+
     }
 
     function handleChange(event) {
@@ -94,17 +97,49 @@ const CardReserve = ({cardTimer}) => {
         return () => clearTimeout(timer);
     }, [time]);
 
+    const handleSubmitNamePhone = (e) => {
+        e.preventDefault();
+
+        const data = {name, phone};
+        fetch('https://your-api.com/save', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+            });
+    };
+    console.log('hit', name, phone)
+
     return (
         <MainReservation>
             <BoxTime>
                 <p>{cardTimer}</p>
             </BoxTime>
-            <Reservations style={{backgroundColor: '#acf232', display: 'flex', flexDirection: 'column'}}>
+            <Reservations
+                onSubmit={handleSubmitNamePhone}
+                style={{backgroundColor: '#acf232', display: 'flex', flexDirection: 'column'}}>
                 {!showSpan && notReservation && (
-                    <div>
-                        <p>Horário disponível</p>
-                        <input type="checkbox" checked={checked} onChange={handleChange}/>
-                        <label>Reservar Horário</label>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}>
+                        <p>Horário Disponível</p>
+                        <span>
+                            <input type="checkbox" checked={checked} onChange={handleChange}
+                                   style={{
+                                       minHeight: '20px',
+                                       width: '20px',
+                                       position: 'relative',
+                                       top: '4px',
+                                       right: '2px'
+                                   }}/>
+                            <label>Reservar Horário</label>
+                        </span>
                     </div>
                 )}
 
@@ -116,7 +151,13 @@ const CardReserve = ({cardTimer}) => {
                         <div style={{display: 'flex', alignItems: 'center', padding: '20px'}}>
                             <AiOutlineUser style={{display: 'flex', padding: '10px 8px 0 0'}}/>
                             <InputMenu>
-                                <input required="required" type="text" pattern="[A-Za-z]{3,}" inputMode="latin-name"/>
+                                <input
+                                    required="required"
+                                    type="text"
+                                    pattern="[A-Za-z]{3,}"
+                                    inputMode="latin-name"
+                                    onChange={(e) => setName(e.target.value)}
+                                />
                                 <span>Nome</span>
                                 <i/>
                             </InputMenu>
@@ -124,25 +165,26 @@ const CardReserve = ({cardTimer}) => {
 
                         <div style={{display: 'flex', alignItems: 'center', padding: '20px'}}>
                             <BsWhatsapp style={{display: 'flex', padding: '10px 8px 0 0'}}/>
-                            <InputMenu>
-                                <input required="required" type="tel" pattern="[0-9]+" inputMode="numeric"
-                                       onChange={handleChangeNumberPhone}
-                                       value={phone}
+                            <InputMenu onChange={handleChangeNumberPhone}>
+                                <input
+                                    required="required"
+                                    type="tel"
+                                    pattern="[0-9]+"
+                                    inputMode="numeric"
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    value={phone}
                                 />
                                 <span>Whatsapp</span>
                                 <i/>
                             </InputMenu>
                         </div>
 
-                        <ButtonSend>
-                            <button onClick={startWhatsappValidation} style={{marginTop: '10px'}}>ENVIAR</button>
-                        </ButtonSend>
-
+                        <ButtonSendRegister onClick={startWhatsappValidation}/>
                     </div>
                 )}
 
                 {confirmWithCode && showSpan && (
-                    <div>
+                    <div style={{padding: '0 10px'}}>
                         <p>Digite o código recebido no seu WhatsApp <BsWhatsapp/></p>
                         <div style={{display: 'flex', flexDirection: 'column', padding: '10px'}}>
                             <input type="text" value={code} onChange={handleCodeChange}/>
@@ -154,7 +196,8 @@ const CardReserve = ({cardTimer}) => {
 
                 {reservedTime && (
                     <div>
-                        <p>Agendamento concluído com sucesso! <TbMoodHappy/></p>
+                        <p>Agendamento concluído com sucesso! </p>
+                        <BsCheckCircle style={{minHeight: '40px', width: '40px'}}/>
                         <div>
                             <p>Agora é só aguardar tranquilamente.</p>
                             <p>Você receberá uma mensagem um pouco antes para te lembrar.</p>
@@ -164,7 +207,6 @@ const CardReserve = ({cardTimer}) => {
                 )}
 
             </Reservations>
-
             {customerWithCompletedAppointment && !notReservation && (
                 <Reservations style={{backgroundColor: '#f64549', minWidth: '100%'}}>
                     <div style={{
@@ -175,7 +217,7 @@ const CardReserve = ({cardTimer}) => {
                         marginRight: '85px'
                     }}>
                         <p>Horário Reservado</p>
-                        <SlClose/>
+                        <GiPadlock style={{minHeight: '40px', width: '40px'}}/>
                     </div>
 
                 </Reservations>
