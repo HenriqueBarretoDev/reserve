f60d97dbd426e54b7e96120d6127747108797ac3
Henrique Barreto <77741199+HenriqueBarretoDev@users.noreply.github.com>
2023-02-02 16:36:09 -0300

    wip

--- reserve-e-corte/src/Components/CardReserve/styles.js
+++ reserve-e-corte/src/Components/CardReserve/styles.js
@@ -27,6 +27,7 @@ export const BoxTime = styled.div`
 `;
 
 export const Reservations = styled.div`
+
   display: flex;
   width: 100%;
   justify-content: center;
@@ -52,3 +53,68 @@ export const Reservations = styled.div`
     border: 1px solid black;
   }
 `;
+
+
+export const InputMenu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 0;
+  position: relative;
+  width: 196px;
+
+  input {
+    position: relative;
+    width: 100%;
+    padding: 20px 10px 10px;
+    background: transparent;
+    outline: none;
+    box-shadow: none;
+    border: none;
+    color: #23242a;
+    font-size: 1em;
+    letter-spacing: 0.05em;
+    transition: 0.5s;
+    z-index: 10;
+  }
+
+  span {
+    position: absolute;
+    left: 0;
+    padding: 20px 10px 10px;
+    font-size: 1em;
+    color: #08152c;
+    letter-spacing: 00.05em;
+    transition: 0.5s;
+    pointer-events: none;
+  }
+
+  input:valid ~ span,
+  input:focus ~ span {
+    color: #fdfefd;
+    transform: translateX(-10px) translateY(-34px);
+    font-size: 0, 75em;
+  }
+
+  i {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+    background: #fdfefd;
+    border-radius: 4px;
+    transition: 0.5s;
+    pointer-events: none;
+    z-index: 9;
+  }
+
+  input:valid ~ i,
+  input:focus ~ i {
+    height: 44px;
+  }
+`
+export const ButtonSend = styled.button`
+  
+
+`
