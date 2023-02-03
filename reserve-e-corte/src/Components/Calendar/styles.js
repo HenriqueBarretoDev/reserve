c7d99361222ae52d5614ba79c6df95320faf34f4
Arielson <aryelson17@gmail.com>
2023-02-03 17:42:15 -0300

    WIP calendário, horário e hora não repetem mais

--- reserve-e-corte/src/Components/Calendar/styles.js
+++ reserve-e-corte/src/Components/Calendar/styles.js
@@ -9,12 +9,23 @@ export const CalendarContent = styled.div`
   .react-calendar__navigation__label {
     pointer-events: none;
   }
+
   .react-calendar__month-view__days__day {
     height: 60px;
     width: 50px;
   }
-  
-  
+
+  .react-calendar__month-view__weekdays__weekday {
+    background-color: lightskyblue;
+  }
+
+  .react-calendar__month-view__weekdays__weekday abbr {
+    color: black;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+  }
+
+
   input {
     background-color: rebeccapurple;
     color: red;
@@ -24,47 +35,80 @@ export const MainCalendar = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10px 10px 100px 10px;
-  background-color: wheat;
+  background-color: yellow;
 
+  .react-calendar .react-calendar__tile--active {
+    background-color: red;
+  }
+
+  .today {
+    background-color: darkred;
+  }
+
+  .custom-input {
+    background-color: #f2f2f2;
+    border: none;
+    color: black;
+    padding: 12px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 5px;
+    background-color: yellowgreen;
+  }
 
   form {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: red;
-  }`
+    background-color: white;
+    height: 150px;
+  }
+`
+
 
 export const TimeSelect = styled.select`
+{
+  height: 40px;
   margin-right: 8px;
   padding: 8px 16px;
   background-color: white;
   border-radius: 4px;
   font-size: 16px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 10px;
   border: none;
-  outline: none;
+  outline: red;
+}
 `;
 
 export const ReserveButton = styled.button`
+  height: 50px;
   padding: 8px 16px;
-  background-color: blue;
-  color: #fff;
+  background-color: rgb(63, 255, 0);
+  color: black;
   border: none;
-  border-radius: 4px;
+  border-radius: 14px;
   cursor: pointer;
   font-size: 16px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
 export const ReservationList = styled.ul`
   margin-top: 16px;
-  padding: 10px;
+  padding: 20px;
   background-color: #f1f1f1;
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: pink;
 `;
 
 export const ReservationItem = styled.li`
   margin-bottom: 8px;
   font-size: 16px;
+  background-color: blue;
+`;
+
+
+export const MessageCalendar = styled.li`
+    background-color: orchid;
 `;
